dd3d2a795250c074edb9bf071b93e5e4fd910f05
Anish Manandhar <anishmdhr01@gmail.com>
2024-03-18 13:19:48 +1100

    Edit check_format_and_convert function to mtch boh formats in single line of code.

--- script.py
+++ script.py
@@ -1,9 +1,3 @@
-# load the phone number data in a file
-# check if phone number exists
-# check correct number formatting
-# convert it into international formatting
-# save it into new file/replace in existing file 
-
 import pandas as pd
 import os
 import re
@@ -13,20 +7,18 @@ import csv
 os.chdir('phone_num_formatting')
 
 # load the data in a file
-
 data = pd.read_csv('sample\\au-500.csv')
 
+# Extract the phone number columns
 phone_data  = data[["phone1","phone2"]]
 
 # Regular expressions for checking phone number formats
-phone1Regex = re.compile(r'0\d{1}-\d{4}-\d{4}')  
-phone2Regex = re.compile(r'0\d{3}-\d{3}-\d{3}')
+phone1Regex = re.compile(r'0\d{1}-\d{4}-\d{4}')  # landline format
+phone2Regex = re.compile(r'0\d{3}-\d{3}-\d{3}')  # Mobile format   
 
 
 def check_format_and_convert(num):
-    if re.match(phone1Regex, num):  # Using re.match instead of re.search
-        return "+61-" + num[1:]  # Convert to international format
-    elif re.match(phone2Regex, num):
+    if re.match(phone1Regex, num) or re.match(phone2Regex, num):  # Using re.match instead of re.search
         return "+61-" + num[1:]  # Convert to international format
     else:
         return None  # Invalid format, return None
@@ -38,19 +30,11 @@ data['phone2_intl'] = phone_data['phone2'].apply(check_format_and_convert)
 # Drop rows with invalid phone numbers
 data = data.dropna(subset=['phone1_intl', 'phone2_intl'])
 
-print(data.head())
-
-
-# save the modified data to a new CSV file
-
-data.to_csv('sample\\formatted_phone_numbers.csv', index=False)
-# Error: phone numbers being calculated as formula in excel file
+# print(data.head())
 
 data.to_excel('sample\\au-500-intl.xlsx', index=False)
 
 # export dataframe to a new csv file
-data.to_csv('sample\\au-500-intl.csv', index=False, quoting=csv.QUOTE_ALL)
-
-
-
+# Error: phone numbers being calculated as formula in excel file
+data.to_csv('sample\\au-500-intl.csv', index=False)
 
